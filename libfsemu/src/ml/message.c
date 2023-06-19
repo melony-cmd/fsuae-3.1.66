f2886759706b443a4b2e798b656f80eb310e7f73
melony-cmd <66537589+melony-cmd@users.noreply.github.com>
2023-06-19 13:03:49 +1200

    named pipe; prints to console anything you give it.

--- libfsemu/src/ml/message.c
+++ libfsemu/src/ml/message.c
@@ -5,28 +5,42 @@
 
  */
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "message.h"
 
-int istack = 0;
+int fd;
+char message[512];
+char old_message[512];
 
 // Initalize Messages
 int init_message(void) {
   printf("Initalize Message\n");
+  char *myfifo = "/tmp/fsuae-message";
+  mkfifo(myfifo,0666);
+  fd = open(myfifo,O_RDONLY|O_NONBLOCK);
 }
 
 // DeInitalize Messages
 int deinit_message(void) {
   printf("Deinitalize Message\n");
+  if(fd) { close(fd); }
 }
 
 // Process Messages
 void process_message_events(void) {
-  if(istack==0) { printf("Listening for Messages\n"); }
-
-  istack++;
-
+  //printf("Listening for Messages\n");
+  read(fd,message,512);
+  if(strlen(message)!=0) {
+    if (strcmp(old_message,message)!=0) {
+      message[strlen(message)-1] = 0;
+      printf("msg:(%d)[%s]\n",strlen(message),message);
+      strcpy(old_message,message);
+    }
+  }
   return;
 }
