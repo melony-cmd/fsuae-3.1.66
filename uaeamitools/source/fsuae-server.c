4e7127309082e7a8579563c14e9812e3d872d5dd
melony-cmd <66537589+melony-cmd@users.noreply.github.com>
2023-06-20 23:00:37 +1200

    fsuae-server launched first Amiga application
    Launched Snoopdos & Dir from a linux terminal. :D

--- uaeamitools/source/fsuae-server.c
+++ uaeamitools/source/fsuae-server.c
@@ -10,6 +10,8 @@
 
 #include <clib/exec_protos.h>
 #include <clib/dos_protos.h>
+#include <dos/dos.h>
+#include <dos/dostags.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -18,9 +20,19 @@
 #include "include/uae-control.h"
 #include "include/uae_pragmas.h"
 
+
+long doCmd(UBYTE *command) {
+  struct TagItem stags[4];
+  stags[0].ti_Tag = SYS_Asynch;
+  stags[0].ti_Data = TRUE;
+  stags[1].ti_Tag = TAG_END;
+  return System(command, stags);
+}
+
 int main(int argc,char *argv[]){
 
-  char	buf[257];
+  char buf[257];
+  char runbuf[1024];
 
   if (!strcmp(argv[1],"/?") || !strcmp(argv[1],"")) {
     printf("fsuae-server v0.1 (c) 2023 T.J.Roughton\n");
@@ -30,9 +42,14 @@ int main(int argc,char *argv[]){
       AmigaRunProgram((UBYTE *)&buf);
       if(strcmp(buf,"null")!=0) {
         printf("{%d} msg:(%d)[%s]\n",strcmp(buf,"null"),strlen(buf),buf);
+        //doCmd(buf);
+        //System(buf,SYS_Asynch);
+        sprintf(runbuf,"Run %s",buf);
+        printf(runbuf);
+        System(runbuf);
       }
       Delay(50);
-      //printf("tick!\n");
+      printf("tick!\n");
     }
   }
   return 0;
